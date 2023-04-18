939329cd96d62b12b146824a00ff27589dd3e9d2
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-04-18 18:20:17 +0300

    Remove binary_search_tree.rb, it isn't needed

--- lib/main.rb
+++ lib/main.rb
@@ -1,11 +1,17 @@
 class Board
     def initialize
-        @layout = Arar.new(8) {Array.new(8)}
+        @layout = Arar.new(8) {Array.new(8) {}}
     end
 end
 
 class Knight
+    attr_accessor :board, :tree
     def initialize(board)
         @board = board
+        @tree = nil
     end
+
+    def build_tree(pos = [0,0], des)
+        arr << pos
+
 end
